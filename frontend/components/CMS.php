1a68a0c6145bcb40489a81f04e3fde9aeff1a3b6
Lucia Hyaning Pergiwati Putri Kinasih <Iput_LH746@mitrais.com>
2018-07-18 10:56:48 +0800

    add converter currency function

--- frontend/components/CMS.php
+++ frontend/components/CMS.php
@@ -54,4 +54,13 @@ class CMS {
         return $count;
     }
 
+    public static function currencyConverter($from, $to, $amount){
+        $url    = file_get_contents('https://free.currencyconverterapi.com/api/v5/convert?q=' . $from . '_' . $to . '&compact=ultra');
+        $json   = json_decode($url, true);
+        $rate   = implode(" ",$json);
+        $total  = $rate * $amount;
+        $rounded = round($total,2); //optional, rounds to a whole number
+        return $rounded; //or return $rounded if you kept the rounding bit from above
+    }
+
 }
