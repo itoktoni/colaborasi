3dc14742517b4601487c83eef271e76830a2c6ea
Hermawan Wicaksono <Hermawan_W009@mitrais.com>
2018-07-19 11:03:23 +0800

    fix bug subcategory filter

--- frontend/controllers/CategoryController.php
+++ frontend/controllers/CategoryController.php
@@ -22,6 +22,8 @@ class CategoryController extends \yii\web\Controller
 
 		$this->view->params['menu'] = 'shop';
 
+		$item_list = new \common\models\search\ProductSearch;
+
 		if ($cats) {
 			$maincats = Category::findOne(['slug' => $cats]);
 
@@ -29,34 +31,30 @@ class CategoryController extends \yii\web\Controller
 			{
 				throw new \yii\web\NotFoundHttpException();
 			}
+
+			
  
 			if ( $maincats && $subcategory )
 			{
+				
 				$subcheck 	= Subcategory::findOne( ['category' => $maincats->id, 'slug' => $subcategory] ); 
 				if (!$subcheck)
 				{
 					throw new \yii\web\NotFoundHttpException();
 				}
 
-				// $item_list 	= Product::find()
-				// 				->join( 'LEFT JOIN', 'product_category', 'product.id = product_category.product')
-				// 				->join( 'LEFT JOIN', 'sub_category', 'sub_category.id = product_category.sub_category' )
-				// 				->where( ['sub_category.id' => $subcheck->id] ); 
-
-				$item_list = new \common\models\search\ProductSearch;
-				$query = $item_list->search(Yii::$app->request->post());
+				$query = $item_list->search(['category' => $maincats->id,'subcategory' => $subcheck]);
+				
 			}
 			else
-			{
-				$item_list 	= Product::find()
-								->join('LEFT JOIN','category','category.id = product.category')
-								->where(['category.id' => $maincats->id]);
+			{	
+
+				$query = $item_list->search(['category' => $maincats->id]);
 			}
 		}
-		else {
-			$item_list = Product::find()
-				->join('LEFT JOIN','sub_category','product.category=sub_category.id')
-				->join('LEFT JOIN','category','category.id=sub_category.category');
+		else 
+		{
+				$query = $item_list->search([]);
 		}
 
 		
